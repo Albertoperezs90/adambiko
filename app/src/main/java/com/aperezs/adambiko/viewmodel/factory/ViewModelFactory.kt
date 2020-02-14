55b55d40142ac11c2bd605b583bfaea1693959b9
Alberto Pérez <alberto.perez@innocv.com>
2020-02-14 03:41:32 +0100

    Remove warning

--- app/src/main/java/com/aperezs/adambiko/viewmodel/factory/ViewModelFactory.kt
+++ app/src/main/java/com/aperezs/adambiko/viewmodel/factory/ViewModelFactory.kt
@@ -6,6 +6,7 @@ import javax.inject.Inject
 
 class ViewModelFactory @Inject constructor(private val viewModelMap: Map<Class<out ViewModel>, @JvmSuppressWildcards ViewModel>) : ViewModelProvider.Factory {
 
+    @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         return viewModelMap[modelClass] as T
     }
