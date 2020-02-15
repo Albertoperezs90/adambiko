36641be5243822cece2045586dcecc6a3b562fa1
Alberto Pérez <alberto.perez@innocv.com>
2020-02-15 01:40:57 +0100

    Add top image with 20% height and remove title bar

--- app/src/main/java/com/aperezs/adambiko/view/entries/EntriesActivity.kt
+++ app/src/main/java/com/aperezs/adambiko/view/entries/EntriesActivity.kt
@@ -1,19 +1,14 @@
 package com.aperezs.adambiko.view.entries
 
-import androidx.lifecycle.ViewModelProviders
 import com.aperezs.adambiko.R
 import com.aperezs.adambiko.databinding.EntriesActivityBinding
 import com.aperezs.adambiko.view.base.BaseActivity
 import com.aperezs.adambiko.view.base.addFragment
-import com.aperezs.adambiko.viewmodel.entries.EntriesViewModel
 
 class EntriesActivity : BaseActivity<EntriesActivityBinding>(R.layout.entries_activity) {
 
-    private lateinit var entriesViewModel: EntriesViewModel
-
     override fun initialize() {
         injectDependencies()
-        entriesViewModel = ViewModelProviders.of(this, viewModelFactory)[EntriesViewModel::class.java]
         addFragment(EntriesFragment::class.java)
     }
 }
