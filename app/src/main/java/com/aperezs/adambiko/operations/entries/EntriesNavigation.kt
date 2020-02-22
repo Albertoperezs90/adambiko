ebb589b202055f30860b8545f918759fd314d95c
Alberto Pérez <alberto.perez@innocv.com>
2020-02-23 00:53:20 +0100

    Format whole project

--- app/src/main/java/com/aperezs/adambiko/operations/entries/EntriesNavigation.kt
+++ app/src/main/java/com/aperezs/adambiko/operations/entries/EntriesNavigation.kt
@@ -5,6 +5,6 @@ import com.aperezs.adambiko.operations.entries.model.EntryUI
 sealed class EntriesNavigation {
 
     data class FullScreen(val drawableResource: Int) : EntriesNavigation()
-    data class ModifyEntry(val entryUI: EntryUI): EntriesNavigation()
+    data class ModifyEntry(val entryUI: EntryUI) : EntriesNavigation()
 
 }
